933314ba68daab202c0aa3d5121730b71076dd62
fitrinaannisaa <70376346+fitrinaannisaa@users.noreply.github.com>
2022-10-30 21:33:36 +0700

    Add files via upload

---
+++ MODUL2 FITRINA/FITRINA_HOME.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD Rental</title>
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+
+<body>
+    
+    <nav class="navbar navbar-dark navbar-expand-lg navbar-light bg-dark">
+        <div class="navbar-collapse justify-content-center" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="FITRINA_BOOKING.php">Booking</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <section id="jeniskendaraan" class="py-5 vh-100">
+        <div class="container text-center">
+            <h2 class="mb-4">WELCOME TO EAD RENT</h2>
+            <div class="row justify-content-center">
+                <div class="col-lg-4 mb-3">
+                    <div class="card py-3">
+                        <img src="https://www.toyota.astra.co.id/sites/default/files/2021-08/1-white.png" class="img-fluid">
+                        <h4>Toyota Rush</h4>
+                        <h6>Rp. 200000 / Day</h6>
+                        <div>7 Kursi</div>
+                        <div>1500 CC</div>
+                        <div>Manual </div>
+                        <div class="card-footer">
+                            <a href="FITRINA_BOOKING.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-3">
+                    <div class="card py-3">
+                        <img src="https://asset.honda-indonesia.com/variants/images/Vos4HUL6XFLHnM2dhTzGbzdNHpvNTnBNBKhZwxc0.png" class="img-fluid">
+                        <h4>Honda Brio</h4>
+                        <h6>Rp. 150000 / Day</h6>
+                        <div>4 Kursi</div>
+                        <div>1200 CC</div>
+                        <div>Manual </div>
+                        <div class="card-footer">
+                            <a href="FITRINA_BOOKING.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 mb-3">
+                    <div class="card py-3">
+                        <img src="https://imgd.aeplcdn.com/664x374/n/nd7mjta_1496295.jpg?q=75" class="img-fluid">
+                        <h4>Toyoya Inova</h4>
+                        <h6>Rp. 500000 / Day</h6>
+                        <div>4 Kursi</div>
+                        <div>2400 CC</div>
+                        <div>Automatic </div>
+                        <div class="card-footer">
+                            <a href="FITRINA_BOOKING.php" class="btn btn-primary">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center bg-light py-3">
+        <a class="text-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">Created by FITRINA ANNISA MUSTADA_1202204233</a>
+    </footer>
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Copyright</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Nama: Fitrina Annisa Mustada<br />
+                    NIM: 1202204233<br />
+                    Kelas: SI4406
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js"
+        integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
